3c0a345818647323a6f1c630236d88790df665d2
kimonkanellos <kimonkanellos@gmail.com>
2024-09-07 20:27:33 +0300

    Added card.py

--- card.py
+++ card.py
@@ -9,3 +9,5 @@ class Card:
 
     def __str__(self):
         return f'{self.suit} of {self.rank}'
+
+    values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8, 'Nine':9, 'Ten':10, 'Jack':11, 'Queen':12, 'King':13, 'Ace':14}
