ae6dfc449320c509832dd52f021faefefb4a61de
kimonkanellos <kimonkanellos@gmail.com>
2024-09-07 20:44:00 +0300

    Imported random Added global tuples suits and ranks

--- card.py
+++ card.py
@@ -1,13 +1,18 @@
 # card.py
 # suit = str, rank = str, value = int
+import random
+
+suits = ('Hearts', 'Diamonds', 'Clubs', 'Spades')
+ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
+values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8, 'Nine':9, 'Ten':10, 'Jack':11, 'Queen':12, 'King':13, 'Ace':14}
 
 class Card:
 
     def __init__(self, suit, rank):
         self.suit = suit
         self.rank = rank
+        self.value = values[self.rank]
 
     def __str__(self):
         return f'{self.suit} of {self.rank}'
 
-values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8, 'Nine':9, 'Ten':10, 'Jack':11, 'Queen':12, 'King':13, 'Ace':14}
