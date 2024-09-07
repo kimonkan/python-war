3cc6a216ad7d8a3ddb79a6a782d9bbf271b66411
kimonkanellos <kimonkanellos@gmail.com>
2024-09-07 20:17:19 +0300

    Added card.py

---
+++ card.py
@@ -0,0 +1,11 @@
+# card.py
+# suit = str, rank = str, value = int
+
+class Card:
+
+    def __init__(self, suit, rank):
+        self.suit = suit
+        self.rank = rank
+
+    def __str__(self):
+        return f'{self.suit} of {self.rank}'
