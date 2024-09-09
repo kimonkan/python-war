e012fdbf37a4db542271c1e7b4598b83adc1cded
Kimon Kanellos <kimonkanellos@gmail.com>
2024-09-09 10:49:08 +0300

    Added class Player Improved readability of code

--- main.py
+++ main.py
@@ -9,27 +9,43 @@ values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8,
 class Card:
 
     def __init__(self, suit, rank):
+
         self.suit = suit
         self.rank = rank
         self.value = values[self.rank]
 
     def __str__(self):
+
         return f'{self.rank} of {self.suit}'
 
 
 class Deck:
+
     def __init__(self):
+
         self.cards = []
 
         for suit in suits:
+
             for rank in ranks:
+
                 #Create card object
                 created_card = Card(suit, rank)
 
                 self.cards.append(created_card)
 
     def shuffle(self):
+
         random.shuffle(self.cards)
 
     def deal(self):
+
         return self.cards.pop()
+
+
+class Player:
+
+    def __init__(self, name):
+
+        self.name = name
+        self.hand = []
